6b1e4c835666cad634e58f2f9bc48c52d9a7232a
juedash <juedashherifi@hotmail.com>
2025-07-22 10:38:19 +0200

    add pattern file for the heading

---
+++ theme/patterns/s-heading-with-last-word-coloured.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: KIP - Heading with Last Word Coloured
+ * Slug:  theme/s-heading-with-last-word-coloured
+ * Categories: bootscore
+ * https://developer.wordpress.org/themes/features/block-patterns/
+ * 
+ * @package Bootscore
+ * @version 6.2.0
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+?>
+
+<h2 class="wp-block-heading">Heading <span class="text-primary">coloured part</span></h2>
