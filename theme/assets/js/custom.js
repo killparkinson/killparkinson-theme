35f09327635d5f9ef81fb45986a003721578a4e8
juedash <juedashherifi@hotmail.com>
2025-08-04 11:31:17 +0200

    remove jjquery solution

--- theme/assets/js/custom.js
+++ theme/assets/js/custom.js
@@ -1,29 +1,29 @@
-jQuery(document).ready(function ($) {
-  function syncAllImageHeights() {
-    if (window.innerWidth >= 992) {
-      $(".hero-section").each(function () {
-        var $section = $(this);
-        var $imageCol = $section.find(".col-lg-6").first();
-        var $textCol = $section.find(".col-lg-6").last();
-        var $image = $imageCol.find("img");
+// jQuery(document).ready(function ($) {
+//   function syncAllImageHeights() {
+//     if (window.innerWidth >= 992) {
+//       $(".hero-section").each(function () {
+//         var $section = $(this);
+//         var $imageCol = $section.find(".col-lg-6").first();
+//         var $textCol = $section.find(".col-lg-6").last();
+//         var $image = $imageCol.find("img");
 
-        if ($image.length && $textCol.length) {
-          var textHeight = $textCol.outerHeight();
-          $image.css({
-            height: textHeight + "px",
-            width: "100%",
-            objectFit: "cover"
-          });
-        }
-      });
-    } else {
-      $(".hero-section .col-lg-6 img").css("height", "");
-    }
-  }
+//         if ($image.length && $textCol.length) {
+//           var textHeight = $textCol.outerHeight();
+//           $image.css({
+//             height: textHeight + "px",
+//             width: "100%",
+//             objectFit: "cover"
+//           });
+//         }
+//       });
+//     } else {
+//       $(".hero-section .col-lg-6 img").css("height", "");
+//     }
+//   }
 
-  syncAllImageHeights();
+//   syncAllImageHeights();
 
-  $(window).on("resize", function () {
-    syncAllImageHeights();
-  });
-});
+//   $(window).on("resize", function () {
+//     syncAllImageHeights();
+//   });
+// });
--- theme/patterns/s-hero-img-left.php
+++ theme/patterns/s-hero-img-left.php
@@ -15,24 +15,20 @@ defined('ABSPATH') || exit;
 ?>
 <!-- wp:group {"tagName":"section","metadata":{"name":"s - Hero image left - bg-body-tertiary py-5 hide-wp-block-classes","categories":["bootscore"],"patternName":"bootscore/section-hero-img-left"},"className":"bg-body-tertiary py-5 hide-wp-block-classes","layout":{"type":"default"}} -->
 <section class="wp-block-group py-5 hide-wp-block-classes">
-    <!-- wp:group {"metadata":{"name":"container"},"className":"container","layout":{"type":"default"}} -->
-    <div class="wp-block-group container">
+    <!-- wp:group {"layout":{"type":"default"}} -->
+    <div class="wp-block-group">
         <!-- wp:group {"metadata":{"name":"c - Hero image left - row align-items-center hide-wp-block-classes","categories":["bootscore"],"patternName":"bootscore/c.hero-img-left"},"className":"row hide-wp-block-classes","layout":{"type":"default"}} -->
-        <div class="wp-block-group row hero-section align-items-center hide-wp-block-classes">
+        <div class="wp-block-group row hero-section align-items-stretch hide-wp-block-classes">
             <!-- wp:group {"metadata":{"name":"col-lg-6 mb-3 mb-lg-0"},"className":"col-lg-6 mb-3 mb-lg-0","layout":{"type":"default"}} -->
-            <div class="wp-block-group col-lg-6 mb-3 mb-lg-0 ">
-                <!-- wp:group {"layout":{"type":"default"} class="h-100"} -->
-                <div class="wp-block-group h-100 ">
-                    <!-- wp:image {"sizeSlug":"full","metadata":{"name":"rounded mb-0"},"className":"rounded mb-0"} -->
-                    <figure class="wp-block-image size-full rounded mb-0"><img
+            <div class="wp-block-group col-xl-6 mb-3 mb-lg-0 ">
+                    <!-- wp:image {"sizeSlug":"full","metadata":{"name":"rounded mb-0"},"className":"rounded mb-0 h-100ss"} -->
+                    <figure class="wp-block-image size-full rounded mb-0 h-100"><img
                             src="https://dummyimage.com/1200x900/6c757d/ffffff" alt="" /></figure>
                     <!-- /wp:image -->
-                </div>
-                <!-- /wp:group -->
             </div>
             <!-- /wp:group -->
             <!-- wp:group {"metadata":{"name":"col-lg-6"},"className":"col-lg-6","layout":{"type":"default"}} -->
-            <div class="wp-block-group col-lg-6">
+            <div class="wp-block-group col-xk-6">
                 <!-- wp:group {"metadata":{"name":" d-flex align-items-center"},"className":"h-100 d-flex align-items-center","layout":{"type":"default"}} -->
                 <div class="wp-block-group d-flex align-items-center p-md-4">
                     <!-- wp:group {"metadata":{"name":"text-block"},"className":"text-block py-6","layout":{"type":"default"}} -->
@@ -46,7 +42,7 @@ defined('ABSPATH') || exit;
                             buttons wrapped in a container. Use it on the page-blanktemplate.</p>
                         <!-- /wp:paragraph -->
                         <!-- wp:paragraph {"metadata":{"name":"mb-0"},"className":"mb-0 d-flex gap-2"} -->
-                        <p class="mb-0 d-flex flex-md-row flex-column gap-2">
+                        <p class="mb-0 d-flex flex-xl-row flex-column w-full gap-2">
                             <button class="btn btn-secondary">Secondary
                                 button</button> <button class="btn icon-end btn-outline-secondary">
                                 Button
