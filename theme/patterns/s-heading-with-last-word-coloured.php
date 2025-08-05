18466bf00ca4729fd4f7ee8b671f5a59f3bb7082
juedash <juedashherifi@hotmail.com>
2025-08-05 11:57:59 +0200

    php lint

--- theme/patterns/s-heading-with-last-word-coloured.php
+++ theme/patterns/s-heading-with-last-word-coloured.php
@@ -4,18 +4,18 @@
  * Slug:  theme/s-heading-with-last-word-coloured
  * Categories: bootscore
  * https://developer.wordpress.org/themes/features/block-patterns/
- * 
+ *
  * @package Bootscore
  * @version 6.2.0
  */
 
 // Exit if accessed directly
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
 ?>
 
 <!-- wp:heading {"metadata":{"name":"display-5 fw-bold"},"className":"display-5 fw-bold"} -->
 <h2 class="wp-block-heading display-5 fw-bold">
-  Hero <span class="text-primary">Title</span>
+	Hero <span class="text-primary">Title</span>
 </h2>
 <!-- /wp:heading -->
--- theme/patterns/s-hero-img-right.php
+++ theme/patterns/s-hero-img-right.php
@@ -3,79 +3,79 @@
  * Title: KiP - Hero image right
  * Slug: theme/s-hero-img-right
  * Categories: bootscore
- * https://developer.wordpress.org/themes/features/block-patterns/ 
- * 
+ * https://developer.wordpress.org/themes/features/block-patterns/
+ *
  * @package Bootscore
  * @version 6.2.0
  */
 
 // Exit if accessed directly
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 ?>
 
 <!-- wp:group {"tagName":"section","metadata":{"name":"s - Hero image left - bg-body-tertiary py-5 hide-wp-block-classes","categories":["bootscore"],"patternName":"bootscore/section-hero-img-left"},"className":"bg-body-tertiary py-5 hide-wp-block-classes","layout":{"type":"default"}} -->
 <section class="wp-block-group py-5 hide-wp-block-classes">
-    <!-- wp:group {"layout":{"type":"default"}} -->
-    <div class="wp-block-group">
-        <!-- wp:group {"metadata":{"name":"c - Hero image left - row align-items-center hide-wp-block-classes","categories":["bootscore"],"patternName":"bootscore/c.hero-img-left"},"className":"row hide-wp-block-classes","layout":{"type":"default"}} -->
-        <div class="wp-block-group row hero-section hide-wp-block-classes">
-            <!-- wp:group {"metadata":{"name":"col-lg-6 mb-3 mb-lg-0"},"className":"col-lg-6 mb-3 mb-lg-0","layout":{"type":"default"}} -->
-            <div class="wp-block-group col-lg-6 mb-3 mb-lg-0 h-full order-lg-2">
-                <!-- wp:image {"sizeSlug":"full","metadata":{"name":"rounded h-full mb-0"},"className":"rounded h-full mb-0"} -->
-                <figure class="wp-block-image h-full rounded mb-0 h-full hero-section-image"><img
-                        src="https://dummyimage.com/1200x900/6c757d/ffffff" alt="" /></figure>
-                <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group {"metadata":{"name":"col-lg-6"},"className":"col-lg-6","layout":{"type":"default"}} -->
-            <div class="wp-block-group col-lg-6 d-flex align-items-center p-md-4 w-full">
-                <!-- wp:group {"metadata":{"name":"text-block"},"className":"text-block py-6","layout":{"type":"default"}} -->
-                <div class="wp-block-group text-block w-full my-4">
-                    <!-- wp:heading {"metadata":{"name":"display-5 fw-bold mb-2"},"className":"display-5 fw-bold"} -->
-                    <h2 class="wp-block-heading display-5 fw-bold mb-2">Section with <span
-                            class="text-primary">hero</span></h2> <!-- /wp:heading -->
+	<!-- wp:group {"layout":{"type":"default"}} -->
+	<div class="wp-block-group">
+		<!-- wp:group {"metadata":{"name":"c - Hero image left - row align-items-center hide-wp-block-classes","categories":["bootscore"],"patternName":"bootscore/c.hero-img-left"},"className":"row hide-wp-block-classes","layout":{"type":"default"}} -->
+		<div class="wp-block-group row hero-section hide-wp-block-classes">
+			<!-- wp:group {"metadata":{"name":"col-lg-6 mb-3 mb-lg-0"},"className":"col-lg-6 mb-3 mb-lg-0","layout":{"type":"default"}} -->
+			<div class="wp-block-group col-lg-6 mb-3 mb-lg-0 h-full order-lg-2">
+				<!-- wp:image {"sizeSlug":"full","metadata":{"name":"rounded h-full mb-0"},"className":"rounded h-full mb-0"} -->
+				<figure class="wp-block-image h-full rounded mb-0 h-full hero-section-image"><img
+						src="https://dummyimage.com/1200x900/6c757d/ffffff" alt="" /></figure>
+				<!-- /wp:image -->
+			</div>
+			<!-- /wp:group -->
+			<!-- wp:group {"metadata":{"name":"col-lg-6"},"className":"col-lg-6","layout":{"type":"default"}} -->
+			<div class="wp-block-group col-lg-6 d-flex align-items-center p-md-4 w-full">
+				<!-- wp:group {"metadata":{"name":"text-block"},"className":"text-block py-6","layout":{"type":"default"}} -->
+				<div class="wp-block-group text-block w-full my-4">
+					<!-- wp:heading {"metadata":{"name":"display-5 fw-bold mb-2"},"className":"display-5 fw-bold"} -->
+					<h2 class="wp-block-heading display-5 fw-bold mb-2">Section with <span
+							class="text-primary">hero</span></h2> <!-- /wp:heading -->
 
-                    <!-- wp:paragraph {"metadata":{"name":"lead"},"className":"lead"} -->
-                    <p class="lead">This section features a two-column hero with a right-aligned image and CTA
-                        buttons wrapped in a container. Use it on the page-blanktemplate.</p>
-                    <!-- /wp:paragraph -->
+					<!-- wp:paragraph {"metadata":{"name":"lead"},"className":"lead"} -->
+					<p class="lead">This section features a two-column hero with a right-aligned image and CTA
+						buttons wrapped in a container. Use it on the page-blanktemplate.</p>
+					<!-- /wp:paragraph -->
 
-                    <!-- wp:list -->
-                    <ul class="wp-block-list">
-                        <!-- wp:list-item -->
-                        <li>List Item 1</li>
-                        <!-- /wp:list-item -->
+					<!-- wp:list -->
+					<ul class="wp-block-list">
+						<!-- wp:list-item -->
+						<li>List Item 1</li>
+						<!-- /wp:list-item -->
 
-                        <!-- wp:list-item -->
-                        <li>List Item 2</li>
-                        <!-- /wp:list-item -->
+						<!-- wp:list-item -->
+						<li>List Item 2</li>
+						<!-- /wp:list-item -->
 
-                        <!-- wp:list-item -->
-                        <li>List Item 3</li>
-                        <!-- /wp:list-item -->
-                    </ul>
-                    <!-- /wp:list -->
+						<!-- wp:list-item -->
+						<li>List Item 3</li>
+						<!-- /wp:list-item -->
+					</ul>
+					<!-- /wp:list -->
 
-                    <!-- wp:paragraph {"metadata":{"name":"mb-0"},"className":"mb-0 d-flex gap-2"} -->
-                    <p class="mb-0 d-flex flex-md-row flex-column gap-2">
-                        <button class="btn btn-secondary">Secondary button</button>
-                        <button class="btn icon-end btn-outline-secondary">
-                            Button
+					<!-- wp:paragraph {"metadata":{"name":"mb-0"},"className":"mb-0 d-flex gap-2"} -->
+					<p class="mb-0 d-flex flex-md-row flex-column gap-2">
+						<button class="btn btn-secondary">Secondary button</button>
+						<button class="btn icon-end btn-outline-secondary">
+							Button
 
-                            <svg class="icon">
-                                <use href="/wp-content/themes/kp-theme/assets/fonts/icon.svg#chevron-right" />
-                            </svg>
-                        </button>
-                    </p>
-                    <!-- /wp:paragraph -->
+							<svg class="icon">
+								<use href="/wp-content/themes/kp-theme/assets/fonts/icon.svg#chevron-right" />
+							</svg>
+						</button>
+					</p>
+					<!-- /wp:paragraph -->
 
-                </div>
-                <!-- /wp:group -->
-            </div>
-            <!-- /wp:group -->
-        </div>
-        <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
+				</div>
+				<!-- /wp:group -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
 </section>
 <!-- /wp:group -->
