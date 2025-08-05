c7d9b15bbfad67989bf017340ad10c67d1f24096
juedash <juedashherifi@hotmail.com>
2025-08-05 10:35:55 +0200

    remove  jqeury commented code

--- theme/assets/js/custom.js
+++ theme/assets/js/custom.js
@@ -1,29 +1,6 @@
-// jQuery(document).ready(function ($) {
-//   function syncAllImageHeights() {
-//     if (window.innerWidth >= 992) {
-//       $(".hero-section").each(function () {
-//         var $section = $(this);
-//         var $imageCol = $section.find(".col-lg-6").first();
-//         var $textCol = $section.find(".col-lg-6").last();
-//         var $image = $imageCol.find("img");
+jQuery(function ($) {
+    // Do stuff here
 
-//         if ($image.length && $textCol.length) {
-//           var textHeight = $textCol.outerHeight();
-//           $image.css({
-//             height: textHeight + "px",
-//             width: "100%",
-//             objectFit: "cover"
-//           });
-//         }
-//       });
-//     } else {
-//       $(".hero-section .col-lg-6 img").css("height", "");
-//     }
-//   }
+    // Do stuff here
 
-//   syncAllImageHeights();
-
-//   $(window).on("resize", function () {
-//     syncAllImageHeights();
-//   });
-// });
+}); // jQuery End
