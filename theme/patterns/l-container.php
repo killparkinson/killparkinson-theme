4f11f57c3bbdf81f9578c9c216e3f5618090c010
juedash <juedashherifi@hotmail.com>
2025-08-26 18:55:03 +0200

    rename custom container

---
+++ theme/patterns/l-container.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: KiP - Container
+ * Slug: theme/l-container
+ * Categories: bootscore
+ * https://developer.wordpress.org/themes/features/block-patterns/
+ *
+ * @package Bootscore
+ * @version 6.2.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+?>
+<!-- wp:group {"metadata":{"name":"l - Container - container hide-wp-block-classes",
+							"categories":["bootscore"],"patternName":"bootscore/l.container"},
+							"className":"container hide-wp-block-classes w-100 w-md-75 mx-auto",
+							"layout":{"type":"default"}} -->
+<div class="wp-block-group w-100 w-md-75 mx-auto hide-wp-block-classes">
+	<!-- wp:paragraph {"className":"placeholder col-12 placeholder-lg bg-primary w-100 h-100"} -->
+	<p class="placeholder placeholder col-12 placeholder-lg">This is a <code>container</code></p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
