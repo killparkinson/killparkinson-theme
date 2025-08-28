7fd391e1e5c8bf0a2b30a63b0de6b326143001e7
juedash <juedashherifi@hotmail.com>
2025-08-28 18:29:51 +0200

    remove template part, update the custom page layout

--- theme/page-templates/page-blank-container.php
+++ theme/page-templates/page-blank-container.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
- * Template Name: Custom Template
+ * Template Name: Page Template with breadcrumbs
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -8,25 +9,29 @@
  * @version 6.0.0
  */
 
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+// Exit if accessed directly
+defined('ABSPATH') || exit;
 
 get_header();
 ?>
 
-	<div id="content" class="site-content <?php apply_filters( 'bootscore/class/container', 'container', 'page-blank-with-container' ); ?>">
-	<div id="primary" class="content-area">
-	<main id="main" class="site-main">
-		<div class="entry-content">
-		<?php get_template_part( 'template-parts/content', 'breadcrumb' ); ?>
-		<?php the_post(); ?>
-		<?php the_content(); ?>
-		</div>
+  <div id="content" class="site-content <?= apply_filters('bootscore/class/container', 'container', 'page-blank-with-container'); ?>">
+    <div id="primary" class="content-area">
 
-	</main>
+      <main id="main" class="site-main">
 
-	</div>
-</div>
+        <div class="entry-content">
+				<?php the_breadcrumb(); ?>
+          <?php the_post(); ?>
+          <?php the_content(); ?>
+        </div>
+
+      </main>
+
+    </div>
+  </div>
 
 <?php
 get_footer();
+
+
