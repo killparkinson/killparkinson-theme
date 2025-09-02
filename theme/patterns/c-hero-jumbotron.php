1efbca36277c30a2d635b43be34c952a740b01dd
Thomas David <tom@mightysmile.com>
2025-09-02 07:49:42 +0200

    feat: add new hero jumbotron pattern

---
+++ theme/patterns/c-hero-jumbotron.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Title: KiP - Hero Jumbotron
+ * Slug: kip-theme/c-kip-hero-jumbotron
+ * Categories: bootscore,call-to-action
+ *
+ * @package Bootscore Child
+ * @version 6.0.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+?>
+
+<!-- wp:group {"tagName":"section", "metadata":{"name":"c - KIP Hero jumbotron","categories":["bootscore","call-to-action"],"patternName":"bootscore/c-kip-hero-jumbotron"},"className":"mb-3 p-4 p-md-5 text-center bg-body-tertiary rounded-3 hide-wp-block-classes","layout":{"type":"default"}} -->
+<section class="wp-block-group mb-3 p-4 p-md-5 text-center bg-body-tertiary rounded-3 hide-wp-block-classes">
+	<!-- wp:group {"metadata":{"name":"d-grid gap-4 col-lg-8 offset-lg-2"},"className":"d-grid gap-4 col-lg-8 offset-lg-2","layout":{"type":"default"}} -->
+	<div class="wp-block-group d-grid gap-4 col-lg-8 offset-lg-2">
+		<!-- wp:heading {"level":1,"metadata":{"name":"display-5 fw-bold colour-last-word"},"className":"display-5 fw-bold colour-last-word"} -->
+		<h1 class="wp-block-heading display-5 fw-bold colour-last-word">Make a <span class="text-primary">donation</span></h1>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph {"metadata":{"name":"lead mb-0"},"className":"lead mb-0"} -->
+		<p class="lead mb-0">Your contribution helps fund groundbreaking research and supports global collaboration to defeat Parkinson’s. Together, we can make a difference.</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:buttons {"className":"d-flex justify-content-center"} -->
+		<div class="wp-block-buttons d-flex justify-content-center">
+			<!-- wp:button {"className":"icon-start-hand-heart"} -->
+			<div class="wp-block-button icon-start-hand-heart">
+				<a class="wp-block-button__link wp-element-button">Donate now</a>
+			</div>
+			<!-- /wp:button -->
+		</div>
+		<!-- /wp:buttons -->
+	</div>
+	<!-- /wp:group -->
+</section>
+<!-- /wp:group -->
